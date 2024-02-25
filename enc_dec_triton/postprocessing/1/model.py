620633188738bff52e10c7ff0cc5f69c71b6fdb4
Kshitiz Gupta <kshitizg@nvidia.com>
2024-02-25 02:26:42 +0000

    add initial version

---
+++ enc_dec_triton/postprocessing/1/model.py
@@ -0,0 +1,54 @@
+import json
+import logging
+import numpy as np 
+import sys
+import os
+import triton_python_backend_utils as pb_utils
+from transformers import AutoTokenizer
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TritonPythonModel:
+    """This model loops through different dtypes to make sure that
+    serialize_byte_tensor works correctly in the Python backend.
+    """
+
+    def initialize(self, args):
+        model_config = json.loads(args['model_config'])
+        tokenizer_dir = model_config['parameters']['tokenizer_dir'][
+            'string_value']
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir,
+                                                       legacy=False,
+                                                       trust_remote_code=True)
+        output0_config = pb_utils.get_output_config_by_name(
+            model_config, "text_output")
+       
+        self.output0_dtype = pb_utils.triton_string_to_numpy(
+            output0_config['data_type'])
+        
+        
+    def execute(self, requests):
+        
+        responses = []
+        for request in requests:
+            
+            output_ids_tensor = pb_utils.get_input_tensor_by_name(request, "output_ids")
+            output_ids = output_ids_tensor.as_numpy()
+            
+
+            outputs = self._postprocessing(output_ids)
+            outputs = np.array(outputs).astype(self.output0_dtype)
+            # prepare response object
+            text_output_tensor  = pb_utils.Tensor("text_output", outputs)
+            responses.append(pb_utils.InferenceResponse([text_output_tensor]))
+
+        return responses
+
+    def _postprocessing(self, tokens_batch):
+        outputs = []
+        for beam_tokens in tokens_batch:
+            for tokens in beam_tokens:
+                output = self.tokenizer.decode(tokens)
+                outputs.append(output.encode('utf8'))
+        return outputs
---
+++ enc_dec_triton/preprocessing/1/model.py
@@ -0,0 +1,55 @@
+import json
+import logging
+import numpy as np 
+import sys
+import os
+import triton_python_backend_utils as pb_utils
+from transformers import AutoTokenizer
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class TritonPythonModel:
+    """This model loops through different dtypes to make sure that
+    serialize_byte_tensor works correctly in the Python backend.
+    """
+
+    def initialize(self, args):
+        model_config = json.loads(args['model_config'])
+        tokenizer_dir = model_config['parameters']['tokenizer_dir'][
+            'string_value']
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir,
+                                                       legacy=False,
+                                                       trust_remote_code=True)
+
+        # Parse model output configs and convert Triton types to numpy types
+        self.output_names = [
+            "input_ids", "attention_mask"
+        ]
+        self.output_dtypes = {}
+        for output_name in self.output_names:
+            self.output_dtypes[output_name] = pb_utils.triton_string_to_numpy(
+                    pb_utils.get_output_config_by_name(
+                        model_config, output_name)['data_type'])
+
+
+    def execute(self, requests):
+                
+        responses = []
+        for request in requests:
+            
+            text_input = pb_utils.get_input_tensor_by_name(request,
+                                                      'text_input').as_numpy()
+            batch_size = text_input.shape[0]                        
+            tok_batch = []
+            for i in range(batch_size):                
+                decoded_object = input_query[i, 0].decode()                                               
+                tok_batch.append(decoded_object)
+                            
+            tok_sent = self.tokenizer(tok_batch, return_tensors="np", padding=True)       
+            output_tensors = [] 
+            for output_name in self.output_names:
+                output = tok_sent.get(output_name).astype(self.output_dtypes[output_name])
+
+            responses.append(pb_utils.InferenceResponse([output_tensors]))
+        return responses
